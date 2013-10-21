42220e0645f7b880e10a342a46f820719c66ccae
Andrea Stagi <stagi.andrea@gmail.com>
2013-10-21 20:01:41 +0200

    Added NimbitsError exception and response parsing

--- nimbits.py
+++ nimbits.py
@@ -1,5 +1,12 @@
 import requests, json
 
+class NimbitsError(Exception):
+  '''Base class for Nimbits errors'''
+
+  @property
+  def message(self):
+    return self.args[0]
+
 class Nimbits:
 
     ninbits_url_template = '%s/service/v2/%s'
@@ -14,15 +21,26 @@ class Nimbits:
 
     def get_tree(self):
         query = {'email':self.email,'key':self.key}
-        return self.__send_request('GET', 'tree', query)
+        req = self.__send_request('GET', 'tree', query)
+        return self.__parse_response(req)
 
     def get_value(self, id_entity):
         query = {'email':self.email,'key':self.key,'id':id_entity}
-        return self.__send_request('GET', 'value', query)
+        req = self.__send_request('GET', 'value', query)
+        return self.__parse_response(req)
 
     def set_value(self, id_entity, **params):
         query = {'email':self.email,'key':self.key,'action':'update','id':id_entity}
-        return self.__send_request('POST', 'value', query, params)
+        req = self.__send_request('POST', 'value', query, params)
+        return self.__parse_response(req)
+
+    def __parse_response(self, req):
+        if req.status_code == 200:
+            return req.json()
+        elif req.status_code == 401:
+            raise NimbitsError('Access denied')
+        else:
+            raise NimbitsError(req.text)
 
     def __build_query(self, params):
         query = ''
@@ -41,7 +59,4 @@ class Nimbits:
             req = requests.post(url=url, data=json.dumps(params), headers=headers)
         elif method == 'GET':
             req = requests.get(url=url, headers=headers)
-        if req.status_code == 200:
-            return req.json()
-        else:
-            return None
+        return req
--- sample.py
+++ sample.py
@@ -1,13 +1,16 @@
-from nimbits import Nimbits
+from nimbits import Nimbits, NimbitsError
 
 email = ''
 key = ''
+
 nimbits_client = Nimbits(email, key)
 entity = "SampleValue"
-nimbits_client.set_value(entity, lt=0.0, lg=0.0, d=3.5, t=1375830638376)
-value = nimbits_client.get_value(entity)
-if value:
+
+try:
+    print nimbits_client.set_value(entity, d=3.6, t=1375830638376)
+    value = nimbits_client.get_value(entity)
     print value['d']
-tree = nimbits_client.get_tree()
-if tree:
+    tree = nimbits_client.get_tree()
     print tree
+except NimbitsError as ne:
+    print ne.message
