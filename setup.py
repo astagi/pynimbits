572d7d32d4e7f6afcfbe50b0c0e75866c67b8c92
Andrea Stagi <stagi.andrea@gmail.com>
2013-10-24 23:32:32 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="libnimbits",
+      py_modules=['nimbits'],
+      version="0.1",
+      description="Python library for Nimbits.com",
+      license="MIT",
+      author="Andrea Stagi",
+      author_email="stagi.andrea@gmail.com",
+      url="https://github.com/astagi/libnimbits",
+      keywords= "nimbits raspberry domotics",
+      zip_safe = True)
